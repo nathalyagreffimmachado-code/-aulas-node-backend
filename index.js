cade1d5440d81922f6c4ba4f4cba099c1e5b42c4
nathalya greffim machado <nathalyagreffimmachado@gmail.com>
2026-07-13 21:29:30 -0300

    aula

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import express from 'express'
  
 // server ou app
 const app = express()
-app.get("/api/v1/somar", (req, res) => {
-const { num1, num2  } = req.query
+
+app.use(express.json())
+
+app.post("/api/v1/somar", (req, res) => {
+const { num1, num2  } = req.body
 const resultado = Number(num1) + Number(num2)
 res.status(200).send({message: resultado})
 
 }) 
 
-app.get("/api/v1/sub", (req, res) => {
-    const { num1, num2  } = req.query
+app.post("/api/v1/sub", (req, res) => {
+    const { num1, num2  } = req.body
 const resultado = Number(num1) - Number(num2)
 res.status(200).send({message: resultado})
 
@@ -30,4 +33,4 @@ res.status(200).send({message: resultado})
 })
 app.listen(3000,() => {
     console.log("Servidor ouvindo na porta 3000")
-})
+})                            
