d7cf1d21e047a1881c8a80cc482efe2b93a4c6d5
nathalya greffim machado <nathalyagreffimmachado@gmail.com>
2026-07-16 20:05:02 -0300

    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cachorro from './router/cachorro.js'
 
  
 // server ou app
@@ -6,31 +7,34 @@ const app = express()
 
 app.use(express.json())
 
-app.post("/api/v1/somar", (req, res) => {
-const { num1, num2  } = req.body
-const resultado = Number(num1) + Number(num2)
-res.status(200).send({message: resultado})
+app.use('/api/v1/cachorro', cachorro)
 
-}) 
+// app.post("/api/v1/somar", (req, res) => {
+// const { num1, num2  } = req.body
+// const resultado = Number(num1) + Number(num2)
+// res.status(200).send({message: resultado})
 
-app.post("/api/v1/sub", (req, res) => {
-    const { num1, num2  } = req.body
-const resultado = Number(num1) - Number(num2)
-res.status(200).send({message: resultado})
+// }) 
 
- })
- app.get("/api/v1/div", (req, res) => { 
-    const { num1, num2  } = req.query
-const resultado = Number(num1) / Number(num2)
-res.status(200).send({message: resultado})
+// app.post("/api/v1/sub", (req, res) => {
+//     const { num1, num2  } = req.body
+// const resultado = Number(num1) - Number(num2)
+// res.status(200).send({message: resultado})
 
- })
-app.get("/api/v1/mult", (req, res) => { 
-     const { num1, num2  } = req.query
-const resultado = Number(num1) * Number(num2)
-res.status(200).send({message: resultado})
+//  })
+//  app.get("/api/v1/div", (req, res) => { 
+//     const { num1, num2  } = req.query
+// const resultado = Number(num1) / Number(num2)
+// res.status(200).send({message: resultado})
 
-})
+//  })
+// app.get("/api/v1/mult", (req, res) => { 
+//      const { num1, num2  } = req.query
+// const resultado = Number(num1) * Number(num2)
+// res.status(200).send({message: resultado})
+
+// })
+
 app.listen(3000,() => {
     console.log("Servidor ouvindo na porta 3000")
 })                            
